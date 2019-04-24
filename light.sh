7daf6a2dc3358360aaf3da218abf46fefbf2b218
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-24 13:02:05 +0700

    Create light.sh

---
+++ light.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+echo Selamat datang kak, Siapa nama kamu? #tulisan keluar
+read nick #membaca yang ditulis
+echo "             . "
+sleep 0.03
+echo "         .   :   .  "
+sleep 0.03
+echo "     '.   .  :  .   .' "
+sleep 0.03
+echo "  ._   '._.-'''-._.'   _. "
+sleep 0.03
+echo "    '-..'         '..-'  "
+sleep 0.03
+echo " --._ /.==.     .==.\ _.-- "
+sleep 0.03
+echo "     ;/_o__\   /_o__\; "
+sleep 0.03
+echo "-----|'     ) (     '|-----"
+sleep 0.03
+echo "    _: \_) (\_/) (_/ ;_ "
+sleep 0.03
+echo " --'  \  '._.=._.'  /  '-- "
+sleep 0.03
+echo "   _.-''.  '._.'  .''-._ "
+sleep 0.03
+echo "  '    .''-.(_).-''.    ' "
+sleep 0.03
+echo "     .'   '  :  '   '. "
+sleep 0.03
+echo "        '    :   ' "
+sleep 0.03
+echo "             ' "
+sleep 0.03
+echo "==============================="
+sleep 2
+echo     "Light Bringer Project"
+sleep 0.7
+echo "==============================="
+echo " Tools Pencegah Penipuan Online "
+
+echo Selamat datang $nick ":)"
+get_url=$(curl -s http://zlucifer.com/api/nik.php)
+cek='curl -s '$get_url # check status
+zlucifer="user-agent: zlucifer"
+response=`curl -H "$zlucifer" -m "60" -s -o /dev/null -w "%{http_code}" $cek`
+if [[ $response != *HACKER* ]]; then
+    echo
+    echo "Website Offline/Restart untuk sementara"
+    echo "Silahkan coba lagi"
+else
+    echo
+    echo "Masukan angka 1/2 yang kamu ingin pilih"
+    echo " 1.Tips"
+    echo " 2.Cek"
+    echo "1/2?"
+    read info
+    if [ $info = "1" ]; then
+        echo "TIPS: "
+        echo " Minta nomor rekening (Atas Nama) dan "
+        echo " NIK penjual untuk di cek keasliannya"
+        echo
+        echo " Penjual tidak mau memberi NIK = Waspada"
+        echo " Nama pemilik NIK tidak muncul = Waspada"
+        echo " Nama pemilik NIK dan Nama pemilik rekening berbeda = Waspada"
+        echo " Nama pemilik NIK sama dengan Nama pemilik rekening = Penjual asli"
+        echo " Meskipun penjual asli, tetap waspada"
+     else
+            echo
+            echo "Silahkan masukan NIK Tujuan"
+            read target # masukin NIK
+            echo
+            echo Apakah NIK $target "sudah benar?"
+            echo Y/n?
+            read confirm
+            echo
+            if [ $confirm = "y" ]; then
+                echo Melakukan Pengecekan NIK $target
+                cek_target=`curl -s $get_url/nik.php?nik=$target`
+                echo -e $cek_target
+            else
+                echo Kesalahan, silahkan coba lagi
+            fi
+     fi
+fi
+echo
+echo "==================================="
+echo " Penyusun  : Henrycko"
+echo " Pendukung : Dzakira Alzena Daiva"
+echo " Tools Milik @zLucifer"
+echo " Dilarang menyalahgunakan tools ini :)"
+echo "==================================="
